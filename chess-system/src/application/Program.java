c7424f4f2597eaac018630ebfafce7c17e92db57
caioopessoa <ccapa@cesar.school>
2022-06-16 10:28:40 -0300

    .

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Hello World!");
+		
+		Board board = new Boar(8, 8);
+		
 	}
 
 }
